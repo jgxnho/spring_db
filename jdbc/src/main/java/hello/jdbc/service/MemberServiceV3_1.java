64fece16fb01f9b579d2b3bc2d602a4e3e715133
gxnho <gxnho@naver.com>
2024-04-10 22:14:21 +0900

    4.10 SpringDB

--- jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -40,17 +40,6 @@ public class MemberServiceV3_1 {
         }
     }
 
-    private static void release(Connection con) {
-        if (con != null) {
-            try {
-                con.setAutoCommit(true); //커넥션 풀 고려
-                con.close();
-            } catch (Exception e) {
-                log.info("error: ", e);
-            }
-        }
-    }
-
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
@@ -62,7 +51,7 @@ public class MemberServiceV3_1 {
 
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
-            throw new IllegalArgumentException("이체 중 예외 발생");
+            throw new IllegalStateException("이체 중 예외 발생");
         }
     }
 }
